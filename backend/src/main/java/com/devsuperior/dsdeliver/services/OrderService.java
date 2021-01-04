e62b37e4c99364358b5fecdef3c67415898b52d5
Bruno Gomes <brunog.vix@gmail.com>
2021-01-04 17:38:31 -0300

    PUT / ORDERS/ID/DELIVERED

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -42,4 +42,13 @@ public class OrderService {
 		return new OrderDTO(order);
 	}
 	
+	// uma alteracao no banco de dados
+	@Transactional
+	public OrderDTO setDelivered(Long id){
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		order = repository.save(order);
+		return new OrderDTO(order);
+	}
+	
 }
