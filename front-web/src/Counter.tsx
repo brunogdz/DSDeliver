49d096e33242bd4240d4f4777abc1ad7d3008658
Bruno Gomes <brunog.vix@gmail.com>
2021-01-07 19:54:13 -0300

    Criando um contador

---
+++ front-web/src/Counter.tsx
@@ -0,0 +1,11 @@
+function Counter(){
+    return(
+        <div>
+            <button>Incrementar</button>
+            <button>Decrementar</button>
+            <h1>Valor do contador:</h1>
+        </div>
+    )
+}
+
+export default Counter;
