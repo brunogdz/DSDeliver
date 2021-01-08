6e382c60c79748b2d1e7fa75b3644718c7f16ee0
Bruno Gomes <brunog.vix@gmail.com>
2021-01-08 15:36:42 -0300

    criado a integração com o backend

--- front-web/src/Orders/StepsHeader.tsx
+++ front-web/src/Orders/StepsHeader.tsx
@@ -1,5 +1,3 @@
-import './styles.css';
-
 function StepsHeader() {
     return (
         <header className="orders-steps-container">
@@ -7,6 +5,16 @@ function StepsHeader() {
                 <h1 className="steps-title">
                    SIGA AS <br />  ETAPAS
                 </h1>
+                <ul className="steps-items">
+                    <li>
+                        <span className="steps-number">1</span>
+                        Selecione os produtos e localização.
+                    </li>
+                    <li>
+                        <span className="steps-number">2</span>
+                        Depois clique em <strong>"ENVIAR PEDIDO"</strong>
+                    </li>
+                </ul>
             </div>
         </header>
     )
