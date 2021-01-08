d114ef0ac76e8cd5c7174b2b0481c1a41e72d4af
Bruno Gomes <brunog.vix@gmail.com>
2021-01-08 01:34:33 -0300

    header da page orders e feito as rotas

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,6 +1,8 @@
 import './styles.css';
 import { ReactComponent as MainImage } from '../img/Imagem.svg'
 import Footer from '../Footer';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
@@ -15,9 +17,9 @@ function Home() {
                             Escolha o seu pedido e em poucos minutos <br />
                     levaremoss na sua porta
                     </h3>
-                        <a href="orders" className="home-btn-order">
+                        <Link to="/orders" className="home-btn-order">
                             FAZER PEDIDO
-                    </a>
+                        </Link>
                     </div>
                     <div className="home-image">
                         <MainImage />
---
+++ front-web/src/Orders/StepsHeader.tsx
@@ -0,0 +1,15 @@
+import './styles.css';
+
+function StepsHeader() {
+    return (
+        <header className="orders-steps-container">
+            <div className="orders-steps-content">
+                <h1 className="steps-title">
+                   SIGA AS <br />  ETAPAS
+                </h1>
+            </div>
+        </header>
+    )
+}
+
+export default StepsHeader;
